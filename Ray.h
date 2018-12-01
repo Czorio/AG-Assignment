5f9e6d9f19a87ac435edf6f5cf4d867d2dae3f71
ntatsisk <k.ntatsis@students.uu.nl>
2018-12-01 22:25:09 +0200

    Added Plane - Changed sphere

--- Ray.h
+++ Ray.h
@@ -8,10 +8,17 @@ struct Hit
 	Material mat;
 };
 
+enum RayType
+{
+	PRIMARY_RAY,
+	SHADOW_RAY
+};
+
 struct Ray
 {
 	vec3 origin;
 	vec3 direction;
+	RayType type = PRIMARY_RAY;
 
 	vec3 operator()(const float t) const
 	{
